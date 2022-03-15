8630bca854d7e5e56ce0b0d81d4b6b70041f83ff
Nikhil chauhan <101538341+ChauhanNikhil7878@users.noreply.github.com>
2022-03-15 09:29:29 +0530

    Update Add_two_no.java

--- Add_two_no.java
+++ Add_two_no.java
@@ -5,13 +5,11 @@ public class Main
         return (x+y);
         
     }
-
-
-	public static void main(String[] args) 
+      public static void main(String[] args) 
 	{   
 	    int result;
 	    Main obj1 = new Main();
 	    result = obj1.sum(10,20);
-		System.out.println("The result is::" + result);
+	    System.out.println("The result is::" + result);
 	}
 }
