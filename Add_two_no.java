7e332713f9f5697ee1f9f23c5c37e59c5f30d5ff
Nikhil chauhan <101538341+ChauhanNikhil7878@users.noreply.github.com>
2022-03-15 09:27:31 +0530

    Add files via upload

---
+++ Add_two_no.java
@@ -0,0 +1,17 @@
+public class Main
+{
+ int sum(int x, int y)
+    {
+        return (x+y);
+        
+    }
+
+
+	public static void main(String[] args) 
+	{   
+	    int result;
+	    Main obj1 = new Main();
+	    result = obj1.sum(10,20);
+		System.out.println("The result is::" + result);
+	}
+}
